15a5f40b89ba26790a533dd548b55a3727a92902
Spenser Hardin <shardin@pillartechnology.com>
2017-09-24 07:40:02 -0500

    insufficient payment - failing test

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -2,8 +2,13 @@ from src.vending_machine import VendingMachine
 import nose.tools as nt
 
 
-class TestVendingMachine():
-    def test_input_coins_checks_if_payment_pays_for_item(self):
+class TestVendingMachine:
+    def test_input_coins_updates_display_when_payment_is_suffice(self):
         vending_machine = VendingMachine('coke', 1)
         vending_machine.insert_payment(1)
         nt.assert_equals(vending_machine.DISPLAY, 'Thank you')
+
+    def test_input_coins_does_not_update_display_when_payment_is_insufficient(self):
+        vending_machine = VendingMachine('coke', 1)
+        vending_machine.insert_payment(.50)
+        nt.assert_equals(vending_machine.DISPLAY, 'Insert Coins')
