d6f443e27e17fdd4e58f5ee555ec36412c1a14a5
Spenser Hardin <shardin@pillartechnology.com>
2017-09-24 21:12:32 -0500

    Refactored

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -11,7 +11,10 @@ class VendingMachine(object):
         self.PAYMENT += payment
         if self._payment_is_sufficient():
             self._update_display()
-            return self.PAYMENT - self.price
+            return self._calculate_change()
+
+    def _calculate_change(self):
+        return self.PAYMENT - self.price
 
     def _payment_is_sufficient(self):
         return self.PAYMENT >= self.price
