9c38ed6edf8471510ee2b95c2d733f589026dc07
Spenser Hardin <shardin@pillartechnology.com>
2017-09-26 19:02:14 -0500

    Adding coin.feature - failing

--- features/steps/coin_steps.py
+++ features/steps/coin_steps.py
@@ -1,18 +1,23 @@
 from behave import *
 
+from src.vending_machine import VendingMachine
+
 use_step_matcher("parse")
+
+THANK_YOU = 'Thank you'
 
 
 @given('I have a "{coin}"')
 def step_impl(context, coin):
-    pass
+    context.quarter = '5.670'
 
 
 @when('I insert a "{coin}" into the vending machine')
 def step_impl(context, coin):
-    pass
+    context.vending_machine = VendingMachine('item', .25)
+    context.vending_machine.insert_coins(coin)
 
 
 @then('the vending machine accepts the "{coin}"')
 def step_impl(context, coin):
-    pass
+    assert context.vending_machine.DISPLAY == THANK_YOU
