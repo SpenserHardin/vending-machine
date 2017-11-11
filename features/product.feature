30c6b55e75d637cf034d900ae91155a994194c49
Spenser Hardin <shardin@pillartechnology.com>
2017-11-10 19:00:17 -0600

    Testing more complicated pricing

--- features/product.feature
+++ features/product.feature
@@ -14,3 +14,18 @@ Feature: Product Feature
     And I have inserted "4" "Quarter"
     Then the product is dispensed
     And the machine displays "Thank you"
+
+  Scenario: Customer orders Candy
+    Given I order "Candy" for ".65"
+    When I select the respective button
+    And I have inserted "2" "Quarter"
+    And I have inserted "1" "Dime"
+    And I have inserted "1" "Nickle"
+    Then the product is dispensed
+    And the machine displays "Thank you"
+
+  Scenario: Customer does not have enough coins to complete transaction
+    Given I order "Candy" for ".65"
+    When I select the respective button
+    And I have inserted "2" "Quarter"
+    Then the machine displays "Insert Coins"
--- features/steps/product_steps.py
+++ features/steps/product_steps.py
@@ -9,9 +9,9 @@ from src.vending_machine import VendingMachine
 use_step_matcher("parse")
 
 
-@given('I order a "{product}" which costs "{price:d}"')
+@given('I order "{product}" for "{price}"')
 def step_impl(context, product, price):
-    product = Product(product, price)
+    product = Product(product, float(price))
     validator = CoinValidator()
     context.vending_machine = VendingMachine(product, validator, product.price)
 
@@ -21,25 +21,19 @@ def step_impl(context):
     pass
 
 
-@then("the product is dispensed")
-def step_impl(context):
-    pass
-
-
-@step('the machine displays "{msg}"')
-def step_impl(context, msg):
-    nt.assert_equals(context.vending_machine.DISPLAY, msg)
-
-
-@step('I have inserted "{amount:d}" "{coin_type}"')
+@when('I have inserted "{amount:d}" "{coin_type}"')
 def step_impl(context, amount, coin_type):
     coin = Coin(COINS[coin_type]['weight'])
     for x in range(amount):
         context.vending_machine.insert_coins(coin)
 
 
-@given('I order "{product}" for "{price}"')
-def step_impl(context, product, price):
-    product = Product(product, float(price))
-    validator = CoinValidator()
-    context.vending_machine = VendingMachine(product, validator, product.price)
+@then("the product is dispensed")
+def step_impl(context):
+    pass
+
+
+@then('the machine displays "{msg}"')
+def step_impl(context, msg):
+    nt.assert_equals(context.vending_machine.DISPLAY, msg)
+
